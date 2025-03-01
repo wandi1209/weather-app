63a5e7789e9c3086d8c24825933269e940d6d5d1
wandi1209 <wandider@gmail.com>
2025-03-01 13:15:11 +0700

    create main screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -10,6 +10,65 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.45,
+              color: Colors.red,
+              child: Image.asset("assets/images/Sun-cloud-angled-rain.svg"),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text("Column 2"),
+                        Text("Column 2"),
+                        Text("Column 2"),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    height: 300,
+                    color: Colors.blue,
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text("Column 2"),
+                        Text("Column 2"),
+                        Text("Column 2"),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    height: 300,
+                    color: Colors.blue,
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text("Column 2"),
+                        Text("Column 2"),
+                        Text("Column 2"),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
